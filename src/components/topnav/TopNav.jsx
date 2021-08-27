abda29b06c6af6792570d306c0a1c3240c2f9bb2
Mai Quang Nhat <nhatmai@carp.vn>
2021-08-27 15:11:46 +0700

    Make TopNav

---
+++ src/components/topnav/TopNav.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+import "./topnav.css"
+
+const Topnav = () => {
+    return (
+        <div className='topnav'>
+            <div className="topnav__search">
+                <input type="text" placeholder='Search here...' />
+                <i className='bx bx-search'></i>
+            </div>
+            <div className="topnav__right">
+                <div className="topnav__right-item">
+                    {/* dropdown here */}
+                    
+                </div>
+                <div className="topnav__right-item">
+                    
+                    {/* dropdown here */}
+                </div>
+                <div className="topnav__right-item">
+                    {/* theme setting */}
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Topnav
